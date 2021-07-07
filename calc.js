2dc81d086514db31d05beeffd46fd83bfe741dda
BrendenMarks <60637376+BrendenMarks@users.noreply.github.com>
2021-07-06 23:40:45 -0600

    start of JS

---
+++ calc.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[data-num]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const clearButton = document.querySelector('[data-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const previousOperandTextElement = document.querySelector('[data-prev-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
