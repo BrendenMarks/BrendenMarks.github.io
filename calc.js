e478ce2f0e85c8ba6d169c91a2562bbc9e65077d
BrendenMarks <60637376+BrendenMarks@users.noreply.github.com>
2021-07-06 23:55:01 -0600

    test

--- calc.js
+++ calc.js
@@ -1,3 +1,37 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {
+        this.currentOperandTextElement = ''
+        this.previousOperandTextElement = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    addNumToScreen(userNumber) {
+        this.currentOperandTextElement = userNumber
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperandTextElement
+    }
+}
+
 const numberButtons = document.querySelectorAll('[data-num]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
@@ -5,3 +39,12 @@ const clearButton = document.querySelector('[data-clear]')
 const deleteButton = document.querySelector('[data-delete]')
 const previousOperandTextElement = document.querySelector('[data-prev-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+         calculator.addNumToScreen(button.innerText)
+         calculator.updateDisplay()
+    })
+})
